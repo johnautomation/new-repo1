b833c06cc319037ac63cd872b84a294c08d14ffd
johnautomation <yohannes_tg@yahoo.com>
2018-01-24 13:07:31 -0800

    First java project

---
+++ src/com/first/java/FirstJavaDemo.java
@@ -0,0 +1,12 @@
+package com.first.java;
+
+public class FirstJavaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("The First output of java");
+
+	}
+
+}
