565099b7b9d78bf53149a8f9aa5f84e39e19a724
johnautomation <33437685+johnautomation@users.noreply.github.com>
2018-01-26 00:37:50 -0800

    Update FirstJavaDemo.java

--- src/com/first/java/FirstJavaDemo.java
+++ src/com/first/java/FirstJavaDemo.java
@@ -4,7 +4,7 @@ public class FirstJavaDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//The first java output
 		System.out.println("The First output of java");
 
 	}
