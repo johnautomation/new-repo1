0adf9e7fdea5db10a6700d7aa389d02ff8186591
johnautomation <yohannes_tg@yahoo.com>
2018-01-26 19:54:50 -0800

    added Indeed.java

--- src/com/first/java/Indeed.java
+++ src/com/first/java/Indeed.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Indeed {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\johnq\\Desktop\\lib\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.indeed.com/");
@@ -14,6 +14,7 @@ public class Indeed {
 		driver.findElement(By.name("q")).clear();
 		driver.findElement(By.name("q")).sendKeys("Qulity Enginner");
 		driver.findElement(By.name("l")).clear();
+		Thread.sleep(2000);
 		driver.findElement(By.name("l")).sendKeys("Oakland, CA");
 		driver.findElement(By.id("fj")).click();
 		System.out.println("The new title is - " + driver.getTitle());
