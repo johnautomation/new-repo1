e05897e564bd6f9fd086714f3f2210c78b317825
johnautomation <yohannes_tg@yahoo.com>
2018-01-28 17:27:16 -0800

    updated indeed.java

--- src/com/first/java/Indeed.java
+++ src/com/first/java/Indeed.java
@@ -1,5 +1,7 @@
 package com.first.java;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,8 +11,10 @@ public class Indeed {
 	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\johnq\\Desktop\\lib\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 		driver.get("https://www.indeed.com/");
-		System.out.println(driver.getTitle());
 		driver.findElement(By.name("q")).clear();
 		driver.findElement(By.name("q")).sendKeys("Qulity Enginner");
 		driver.findElement(By.name("l")).clear();
@@ -18,6 +22,18 @@ public class Indeed {
 		driver.findElement(By.name("l")).sendKeys("Oakland, CA");
 		driver.findElement(By.id("fj")).click();
 		System.out.println("The new title is - " + driver.getTitle());
+		String title=driver.getTitle();
+		//Validation point
+		if(title.equals("Quality Engineer Jobs, Employment in Oakland, CA | Indeed.com")){
+		
+			System.out.println("Title is matched");
+		}
+		else{
+			
+		
+			System.out.println("Title is not matched");
+		}
+		
 		driver.quit();
 		
 
