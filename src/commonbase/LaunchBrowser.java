8fad1529e28dbb3efe74091e6b6dd326a8b0ac9e
johnautomation <yohannes_tg@yahoo.com>
2018-02-11 23:31:45 -0800

    page object model added

---
+++ src/commonbase/LaunchBrowser.java
@@ -0,0 +1,12 @@
+package commonbase;
+
+import org.openqa.selenium.WebDriver;
+
+public class LaunchBrowser {
+	public WebDriver driver;
+
+	public LaunchBrowser(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver = driver;
+	}
+}
